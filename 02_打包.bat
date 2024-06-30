3708e0c55e79775df17a1e4a88b300ff087435e0
neavo <neavo@neavo.me>
2024-07-01 03:20:35 +0800

    调整 - 修改了打包后生成的文件名

--- 02_打包.bat
+++ 02_打包.bat
@@ -5,17 +5,17 @@
 
 	@REM 清理环境
 	rd /s /q build
-	del /q main.spec
+	del /q *.spec
 
 	@REM 安装依赖
 	pip install -r requirements.txt
 
 	@REM 执行打包
-	pyinstaller --distpath .\ --clean --noconfirm --onefile main.py
+	pyinstaller --distpath .\ --name KeywordGacha --clean --noconfirm --onefile main.py
 
 	@REM 清理环境
 	rd /s /q build
-	del /q main.spec
+	del /q *.spec
 	
 	@REM 退出虚拟环境
 	call conda deactivate
