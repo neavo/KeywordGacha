4f95f2f3dacb20abb21cf98f71b71659589a46f4
neavo <neavo@neavo.me>
2024-07-06 01:36:09 +0800

    fix typo

--- model/LLM.py
+++ model/LLM.py
@@ -242,7 +242,7 @@ class LLM:
             texts_successed.append(text)
             LogHelper.info(f"[LLM 分词] 已完成 {len(texts_successed)} / {len(texts)} ...")       
         except Exception as error:
-            LogHelper.error(f"[LLM 分词] 子任务执行失败，稍后将重试 ... {error}")
+            LogHelper.warning(f"[LLM 分词] 子任务执行失败，稍后将重试 ... {error}")
 
     # 批量执行分词任务的具体实现
     async def do_extract_words_batch(self, texts, fulltext, words, texts_failed, texts_successed):
@@ -304,7 +304,7 @@ class LLM:
             words_successed.append(word)
             LogHelper.info(f"[词汇翻译] 已完成 {len(words_successed)} / {len(words)} ...")       
         except Exception as error:
-            LogHelper.error(f"[词汇翻译] 子任务执行失败，稍后将重试 ... {error}")
+            LogHelper.warning(f"[词汇翻译] 子任务执行失败，稍后将重试 ... {error}")
 
         # 此处需要直接修改原有的数组，而不能创建新的数组来赋值
         words_failed.clear()
@@ -377,7 +377,7 @@ class LLM:
             words_successed.append(word)
             LogHelper.info(f"[上下文翻译] 已完成 {len(words_successed)} / {len(words)} ...")       
         except Exception as error:
-            LogHelper.error(f"[上下文翻译] 子任务执行失败，稍后将重试 ... {error}")
+            LogHelper.warning(f"[上下文翻译] 子任务执行失败，稍后将重试 ... {error}")
 
         # 此处需要直接修改原有的数组，而不能创建新的数组来赋值
         words_failed.clear()
@@ -439,10 +439,8 @@ class LLM:
             pairs_successed.append(pair)
             LogHelper.info(f"[重复词根检测] 已完成 {len(pairs_successed)} / {len(pairs)} ...")       
         except Exception as error:
-            LogHelper.error(f"[重复词根检测] 子任务执行失败，稍后将重试 ... {error}")
+            LogHelper.warning(f"[重复词根检测] 子任务执行失败，稍后将重试 ... {error}")
  
-     # 实际执行重复词根检测任务
-
     # 批量执行重复词根检测任务的具体实现
     async def do_detect_duplicate_task(self, pairs, pairs_failed, pairs_successed):
         if len(pairs_failed) == 0:
@@ -538,7 +536,7 @@ class LLM:
             words_successed.append(word)
             LogHelper.info(f"[智能总结] 已完成 {len(words_successed)} / {len(words)} ...")       
         except Exception as error:
-            LogHelper.error(f"[智能总结] 子任务执行失败，稍后将重试 ... {error}")
+            LogHelper.warning(f"[智能总结] 子任务执行失败，稍后将重试 ... {error}")
 
     # 批量执行智能总结任务的具体实现
     async def do_summarize_context_batch(self, words, words_failed, words_successed):
