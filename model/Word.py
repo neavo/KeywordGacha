9e63b39dd72a7ac52b2bb8f94575650c7f8b6360
neavo <neavo@neavo.me>
2024-07-08 10:42:45 +0800

    调整 - 为查找上下文加入缓存机制以提升速度

--- model/Word.py
+++ model/Word.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+from threading import Lock, Thread
 from collections import Counter
 from collections import OrderedDict
 
@@ -8,10 +9,16 @@ import tiktoken
 import tiktoken_ext
 from tiktoken_ext import openai_public
 
+from helper.LogHelper import LogHelper
+from helper.TextHelper import TextHelper
+
 class Word:
 
     TYPE_PERSON = 1
     CONTEXT_TOKEN_THRESHOLD = 1024
+
+    CONTEXT_CACHE = {}
+    CONTEXT_CACHE_LOCK = Lock()
 
     def __init__(self):
         self.type = 0
@@ -40,26 +47,33 @@ class Word:
 
     # 从原文中提取上下文
     def set_context(self, surface, original):
-        # 匹配原文
-        matches = [line.strip() for line in original if surface in line]
-        
-        # 使用OrderedDict去除重复并保持顺序
-        unique_matches = list(OrderedDict.fromkeys(matches))
-        
-        # 按长度降序排序
-        unique_matches.sort(key=lambda x: (-len(self.tiktoken_encoding.encode(x)), x))
+        if surface in Word.CONTEXT_CACHE:
+            with CONTEXT_CACHE_LOCK:
+                self.context = Word.CONTEXT_CACHE.get(surface)
+        else:
+            # 匹配原文
+            matches = [line.strip() for line in original if surface in line]
+            
+            # 使用OrderedDict去除重复并保持顺序
+            unique_matches = list(OrderedDict.fromkeys(matches))
+            
+            # 按长度降序排序
+            unique_matches.sort(key=lambda x: (-len(self.tiktoken_encoding.encode(x)), x))
 
-        # 在阈值范围内取 Token 最长的条数
-        context = []
-        context_length = 0
-        for k, line in enumerate(unique_matches):
-            line_lenght = len(self.tiktoken_encoding.encode(line))
+            # 在阈值范围内取 Token 最长的条数
+            context = []
+            context_length = 0
+            for k, line in enumerate(unique_matches):
+                line_lenght = len(self.tiktoken_encoding.encode(line))
 
-            if context_length + line_lenght > self.CONTEXT_TOKEN_THRESHOLD:
-                break
+                if context_length + line_lenght > self.CONTEXT_TOKEN_THRESHOLD:
+                    break
 
-            context.append(line)
-            context_length = context_length + line_lenght
+                context.append(line)
+                context_length = context_length + line_lenght
 
-        # 赋值给 self.context
-        self.context = context
+            self.context = context
+
+            # 将结果保存到缓存中
+            with CONTEXT_CACHE_LOCK:
+                Word.CONTEXT_CACHE[surface] = context
