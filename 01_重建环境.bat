91a58bc95a52e7fc841f0de45d6c619bbd9ecf74
neavo <neavo@neavo.me>
2024-07-12 12:37:03 +0800

    update script

--- 01_重建环境.bat
+++ 01_重建环境.bat
@@ -4,11 +4,17 @@
     call conda create --name KeywordGacha python=3.12 --yes
     call conda activate KeywordGacha
 
+    call python -m pip install --upgrade pip
     call pip install -r requirements.txt
     call python -m spacy download ja_core_news_lg
 
-    rd /s /q dist
-    rd /s /q envs
+    rd /S /Q dist
+    rd /S /Q envs
+
+    @REM Github Action 路径
+    @REM xcopy "C:\Miniconda\envs\KeywordGacha" "envs" /E /I /H /Y
+
+    @REM 本机路径
     xcopy "%USERPROFILE%\miniconda3\envs\KeywordGacha" "envs" /E /I /H /Y
 
     @REM xcopy "envs" "dist\KeywordGacha\envs" /E /I /H /Y
@@ -16,10 +22,10 @@
     @REM xcopy "helper" "dist\KeywordGacha\helper" /E /I /H /Y
     @REM xcopy "prompt" "dist\KeywordGacha\prompt" /E /I /H /Y
 
-    @REM copy /y "00_启动.bat" "dist\KeywordGacha\"
-    @REM copy /y "main.py" "dist\KeywordGacha\"
-    @REM copy /y "config.json" "dist\KeywordGacha\"
-    @REM copy /y "blacklist.txt" "dist\KeywordGacha\"
+    @REM copy "00_启动.bat" "dist\KeywordGacha\"
+    @REM copy "main.py" "dist\KeywordGacha\"
+    @REM copy "config.json" "dist\KeywordGacha\"
+    @REM copy "blacklist.txt" "dist\KeywordGacha\"
 
     call conda deactivate
     call conda env remove --name KeywordGacha --yes
