a9941d1cf7d437d0c12f8b2e4ea627d23c31daa0
neavo <neavo@neavo.me>
2024-09-21 14:49:48 +0800

    kg_ner_20240912

--- 99_打包.bat
+++ 99_打包.bat
@@ -25,21 +25,14 @@
     .\env\python.exe -m pip cache purge
 
     @REM 部署模型
-    .\resource\aria2c.exe https://github.com/neavo/KeywordGachaModel/releases/download/kg_ner_20240826/kg_ner_cpu.zip -o kg_ner_cpu.zip
-    powershell -Command "Expand-Archive -Path 'kg_ner_cpu.zip' -DestinationPath 'dist\KeywordGacha\resource\kg_ner_cpu'"
+    .\resource\aria2c.exe https://github.com/neavo/KeywordGachaModel/releases/download/kg_ner_20240912/kg_ner_cpu.zip -o kg_ner_cpu.zip
+    powershell -Command "Remove-Item -Path 'resource\kg_ner_cpu' -Recurse -Force -ErrorAction SilentlyContinue"
+    powershell -Command "Expand-Archive -Path 'kg_ner_cpu.zip' -DestinationPath 'resource\kg_ner_cpu'"
     powershell -Command "Remove-Item -Path 'kg_ner_cpu.zip' -Recurse -Force -ErrorAction SilentlyContinue"
 
-    @REM 复制文件
-    xcopy "env" ".\dist\KeywordGacha\env" /E /I /Q /H /Y
-    xcopy "model" ".\dist\KeywordGacha\model" /E /I /Q /H /Y
-    xcopy "helper" ".\dist\KeywordGacha\helper" /E /I /Q /H /Y
-    xcopy "prompt" ".\dist\KeywordGacha\prompt" /E /I /Q /H /Y
-
-    copy "01_启动.bat" ".\dist\KeywordGacha\"
-    copy "main.py" ".\dist\KeywordGacha\"
-    copy "config.json" ".\dist\KeywordGacha\"
-    copy "version.txt" ".\dist\KeywordGacha\"
-    copy "blacklist.txt" ".\dist\KeywordGacha\"
-    copy "libomp140.x86_64.dll" ".\dist\KeywordGacha\"
+    .\resource\aria2c.exe https://github.com/neavo/KeywordGachaModel/releases/download/kg_ner_20240912/kg_ner_gpu.zip -o kg_ner_gpu.zip
+    powershell -Command "Remove-Item -Path 'resource\kg_ner_gpu' -Recurse -Force -ErrorAction SilentlyContinue"
+    powershell -Command "Expand-Archive -Path 'kg_ner_gpu.zip' -DestinationPath 'resource\kg_ner_gpu'"
+    powershell -Command "Remove-Item -Path 'kg_ner_gpu.zip' -Recurse -Force -ErrorAction SilentlyContinue"
 
 pause
