06117c7f0dc3b4c126d161f09ec80fe9fa100cb7
neavo <neavo@neavo.me>
2025-06-11 15:29:42 +0800

    minor

--- module/File/FileManager.py
+++ module/File/FileManager.py
@@ -66,53 +66,56 @@ class FileManager(Base):
 
     # 导出
     def write_to_path(self, glossary: list[dict[str, str | int | list[str]]]) -> None:
-        # 新建工作表
-        book: openpyxl.Workbook = openpyxl.Workbook()
-        sheet: openpyxl.worksheet.worksheet.Worksheet = book.active
+        try:
+            # 新建工作表
+            book: openpyxl.Workbook = openpyxl.Workbook()
+            sheet: openpyxl.worksheet.worksheet.Worksheet = book.active
 
-        # 设置表头
-        sheet.column_dimensions["A"].width = 32
-        sheet.column_dimensions["B"].width = 32
-        sheet.column_dimensions["C"].width = 32
-        sheet.column_dimensions["D"].width = 32
-        sheet.column_dimensions["E"].width = 32
-        TableManager.set_cell_value(sheet, 1, 1, "src", 10)
-        TableManager.set_cell_value(sheet, 1, 2, "dst", 10)
-        TableManager.set_cell_value(sheet, 1, 3, "info", 10)
-        TableManager.set_cell_value(sheet, 1, 4, "regex", 10)
-        TableManager.set_cell_value(sheet, 1, 5, "count", 10)
+            # 设置表头
+            sheet.column_dimensions["A"].width = 32
+            sheet.column_dimensions["B"].width = 32
+            sheet.column_dimensions["C"].width = 32
+            sheet.column_dimensions["D"].width = 32
+            sheet.column_dimensions["E"].width = 32
+            TableManager.set_cell_value(sheet, 1, 1, "src", 10)
+            TableManager.set_cell_value(sheet, 1, 2, "dst", 10)
+            TableManager.set_cell_value(sheet, 1, 3, "info", 10)
+            TableManager.set_cell_value(sheet, 1, 4, "regex", 10)
+            TableManager.set_cell_value(sheet, 1, 5, "count", 10)
 
-        # 启用表头筛选
-        sheet.auto_filter.ref = "A1:E1"
+            # 启用表头筛选
+            sheet.auto_filter.ref = "A1:E1"
 
-        # 将数据写入工作表
-        for row, item in enumerate(glossary):
-            TableManager.set_cell_value(sheet, row + 2, 1, item.get("src", ""), 10)
-            TableManager.set_cell_value(sheet, row + 2, 2, item.get("dst", ""), 10)
-            TableManager.set_cell_value(sheet, row + 2, 3, item.get("info", ""), 10)
-            TableManager.set_cell_value(sheet, row + 2, 4, item.get("regex", ""), 10)
-            TableManager.set_cell_value(sheet, row + 2, 5, item.get("count", 0), 10)
+            # 将数据写入工作表
+            for row, item in enumerate(glossary):
+                TableManager.set_cell_value(sheet, row + 2, 1, item.get("src", ""), 10)
+                TableManager.set_cell_value(sheet, row + 2, 2, item.get("dst", ""), 10)
+                TableManager.set_cell_value(sheet, row + 2, 3, item.get("info", ""), 10)
+                TableManager.set_cell_value(sheet, row + 2, 4, item.get("regex", ""), 10)
+                TableManager.set_cell_value(sheet, row + 2, 5, item.get("count", 0), 10)
 
-        # 保存工作簿
-        book.save(f"{self.config.output_folder}/output.xlsx")
+            # 保存工作簿
+            book.save(f"{self.config.output_folder}/output.xlsx")
 
-        # 保存日志
-        with open(f"{self.config.output_folder}/output_log.txt", "w", encoding = "utf-8") as writer:
-            for entry in glossary:
-                src: str = entry.get("src", "")
-                dst: str = entry.get("dst", "")
-                count: int = entry.get("count", 0)
-                context: list[str] = entry.get("context", [])[:10]
+            # 保存日志
+            with open(f"{self.config.output_folder}/output_log.txt", "w", encoding = "utf-8") as writer:
+                for entry in glossary:
+                    src: str = entry.get("src", "")
+                    dst: str = entry.get("dst", "")
+                    count: int = entry.get("count", 0)
+                    context: list[str] = entry.get("context", [])[:10]
 
-                # 写入文件
-                writer.write(f"{Localizer.get().ner_output_log_src}{src}" + "\n")
-                writer.write(f"{Localizer.get().ner_output_log_dst}{dst}" + "\n")
-                writer.write(f"{Localizer.get().ner_output_log_count}{count}" + "\n")
-                writer.write(f"{Localizer.get().ner_output_log_context}※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※" + "\n")
-                writer.write("\n".join(context) + "\n")
-                writer.write("\n")
+                    # 写入文件
+                    writer.write(f"{Localizer.get().ner_output_log_src}{src}" + "\n")
+                    writer.write(f"{Localizer.get().ner_output_log_dst}{dst}" + "\n")
+                    writer.write(f"{Localizer.get().ner_output_log_count}{count}" + "\n")
+                    writer.write(f"{Localizer.get().ner_output_log_context}※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※" + "\n")
+                    writer.write("\n".join(context) + "\n")
+                    writer.write("\n")
 
-        # 保存 JSON
-        [v.pop("context", None) for v in glossary]
-        with open(f"{self.config.output_folder}/output.json", "w", encoding = "utf-8") as writer:
-            writer.write(json.dumps(glossary, indent = 4, ensure_ascii = False))
+            # 保存 JSON
+            [v.pop("context", None) for v in glossary]
+            with open(f"{self.config.output_folder}/output.json", "w", encoding = "utf-8") as writer:
+                writer.write(json.dumps(glossary, indent = 4, ensure_ascii = False))
+        except Exception as e:
+            self.error(f"{Localizer.get().log_read_file_fail}", e)
